9b5d57ba9ee198baf24c816f000c7bbeb887bad6
reneegelderloos <84631679+reneegelderloos@users.noreply.github.com>
2021-06-21 20:11:47 +0200

    Add files via upload

---
+++ eerste_opdracht/script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy")
+let name = "renee";
+console.log("renee")
+
+
+var y = 4;
+var x = 4
+console.log(y + x)
+
+let age = "25"
+
+var y = 4;
+var x = 4;
+console.log(4 * 4)
